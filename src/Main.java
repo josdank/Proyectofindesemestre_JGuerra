2bf219d4ff1c06a3c0e780459735090029d97f5a
josdank <josue.guerra@epn.edu.ec>
2024-07-29 16:19:00 -0500

    res

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
+        System.out.println("alfin valio restaurar");
         JFrame frame = new JFrame("Login");
         frame.setContentPane(new login().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
