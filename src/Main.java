225307f4563988b42548b0c61970e6ffc40138b1
josdank <josue.guerra@epn.edu.ec>
2024-07-31 02:16:18 -0500

    Inserción de Librerias

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         // Crear la ventana de login
         JFrame loginFrame = new JFrame("Login");
-        loginFrame.setContentPane(new stock().mainPanel2);
+        loginFrame.setContentPane(new login().mainPanel);
         loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loginFrame.pack();
         loginFrame.setLocationRelativeTo(null);
